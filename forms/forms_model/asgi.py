1f04db7be5b799b372877c4a107cf9ce6c6cfea4
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-01-19 23:30:12 +0000

    asgi & wsgi file

---
+++ forms/forms_model/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for forms_model project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forms_model.settings')
+
+application = get_asgi_application()

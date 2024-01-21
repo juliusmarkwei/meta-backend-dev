d2ab4da932853041462fe5172df25334c4c8f701
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-01-21 23:26:13 +0000

    added shelf model

--- forms/myapp/models.py
+++ forms/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Shelf(models.Model):
+    id = models.UUIDField(primary_key=True, editable=False)
+    medicine = models.CharField(max_length=200, unique=True)
+    price = models.FloatField(verbose_name="Price for the medicine")
+    add_date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.medicine

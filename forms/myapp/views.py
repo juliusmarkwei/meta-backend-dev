4f20edf3b57e3d13b3dff40b6a8c431013d3ca0d
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-01-25 16:10:26 +0000

    created home view

--- forms/myapp/views.py
+++ forms/myapp/views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request, name="Julius"):
+    String = 'Simple is better than complex' 
+    context = {"name": name, "nums": [1, 2, 3, 4, 5], "string": String}
+    return render(request, 'myapp/hello.html', context)

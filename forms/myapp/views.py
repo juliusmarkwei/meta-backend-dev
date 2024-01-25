beeb21d828d4d3b7d2d594bbcee5494e94f62fcb
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-01-25 17:42:38 +0000

    home, login & register views

--- forms/myapp/views.py
+++ forms/myapp/views.py
@@ -6,3 +6,12 @@ def index(request, name="Julius"):
     String = 'Simple is better than complex' 
     context = {"name": name, "nums": [1, 2, 3, 4, 5], "string": String}
     return render(request, 'myapp/hello.html', context)
+
+def login(request):
+    return render(request, 'myapp/login.html', {})
+
+def register(request):
+    return render(request, 'myapp/register.html', {})
+
+def home(request):
+    return render(request, 'myapp/home.html', {})

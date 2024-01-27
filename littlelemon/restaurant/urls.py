1c972e9b0ad9ad7b9b480b21acc0f2624299c49b
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-01-27 19:35:04 +0000

    little lemos url configs

---
+++ littlelemon/restaurant/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('book/', views.book, name="book"),
+    path('menu/', views.menu, name="menu"),
+    path('menu_item/<int:pk>', views.display_menu_items, name="menu_item"),
+]

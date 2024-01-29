709d563cc1498afd5b529198d8923c0e2a8d6d11
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-01-29 19:08:36 +0000

    views for apis

--- apis/littleLemonAPI/views.py
+++ apis/littleLemonAPI/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def display_even_numbers(request):
+    response = ""
+    numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in numbers:
+        remainder = i % 2
+        if remainder == 0:
+            response += str(i) + "<br/>"
+            
+    return HttpResponse(response)

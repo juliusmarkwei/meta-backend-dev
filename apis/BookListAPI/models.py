650d901cac17b2bb7ddc62794f8b5620336f5856
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-04 18:12:37 +0000

    added category & book models

--- apis/BookListAPI/models.py
+++ apis/BookListAPI/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+class Category(models.Model):
+    slug = models.SlugField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    
+    def __str__(self) -> str:
+        return str(self.name)
+    
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
+
+class Books(models.Model):
+    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    published_date = models.DateField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True, default=None)
+    
+    def __str__(self) -> str:
+        return f'{str(self.author)} - {str(self.title)}'
+    
+    class Meta:
+        verbose_name_plural = 'Books'

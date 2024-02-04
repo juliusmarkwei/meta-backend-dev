7636339bc2706da821bbe4b42c677713fb0b942a
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-04 18:14:58 +0000

    settings for apis

--- apis/littleLemon/settings.py
+++ apis/littleLemon/settings.py
@@ -130,3 +130,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     '127.0.0.1'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+        'rest_framework_xml.renderers.XMLRenderer',
+        'rest_framework_csv.renderers.CSVRenderer',
+        'rest_framework_yaml.renderers.YAMLRenderer',
+    ],
+}

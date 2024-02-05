339b7f468434b6a69578713881c6630b22a82ac2
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-05 20:24:07 +0000

    enppoints for djoser and jwt

--- apis/BookListAPI/urls.py
+++ apis/BookListAPI/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from .views import books, BookList, singlebook, category_detail
+from .views import books, BookList, singlebook, category_detail, secret, throttle_check, throttle_check_auth
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('books', books, name='books'),
     path('books/<int:id>', singlebook, name='singlebook'),
     path('category/<int:id>', category_detail, name='category-detail'),
+    path('secret', secret, name='secret'),
+    path('obtain-auth-token/', obtain_auth_token, name='obtain_auth_token'),
+    path('throttle-check', throttle_check, name='throttle-check'),
+    path('throttle_check_auth', throttle_check_auth, name='throttle_check_auth'),
 ]

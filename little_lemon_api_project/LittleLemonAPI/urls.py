3746a0ef57e9f22b414ebf9d9d625af96ea45193
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-06 19:47:04 +0000

    updated endpoint for carts and groups

--- little_lemon_api_project/LittleLemonAPI/urls.py
+++ little_lemon_api_project/LittleLemonAPI/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('menu-items', views.menu_item, name='menu_item'),
     path('menu-items/<int:menuItem>', views.menu_item_detail, name='menu_item_detail'),
-    
+    path('groups/manager/users', views.manage_managers_group, name='manage_managers_group'),
+    path('groups/manager/users/<int:userId>', views.remove_manager, name='remove_manager'),
+    path('groups/delivery-crew/users', views.manage_delivery_crew_group, name='manage_delivery_crew_group'),
+    path('groups/delivery-crew/users/<int:userId>', views.remove_delivery_crew, name='remove_delivery_crew'),
+    path('cart/menu-items', views.cart_menu_item, name='cart_menu_item'),
 ]

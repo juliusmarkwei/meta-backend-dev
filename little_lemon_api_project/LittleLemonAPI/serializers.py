cba7ffa4ce28229be5de3415b73527a6cdc0992c
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-06 19:46:36 +0000

    added serializers for cart model

--- little_lemon_api_project/LittleLemonAPI/serializers.py
+++ little_lemon_api_project/LittleLemonAPI/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MenuItem 
+from .models import MenuItem, Cart
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,4 +16,24 @@ class MenuItemSerializer(serializers.ModelSerializer):
         
         if data['price'] < 0:
             raise serializers.ValidationError({'price': 'Price cannot be negative'})
+        return data
+    
+
+class CartSerializer(serializers.ModelsSerializer):
+    class Meta:
+        model = Cart
+        fields = ['id', 'user', 'menuitem', 'quantity', 'unit_price', 'price']
+        read_only_fields = ['id']
+
+    def validate(self, data):
+        required_fields = [ 'user', 'menuitem', 'quantity', 'unit_price', 'price']
+        
+        for field in required_fields:
+            if field not in data:
+                raise serializers.ValidationError({field: 'This field is required'})
+        
+        if data['price'] < 0:
+            raise serializers.ValidationError({'price': 'Price cannot be negative'})
+        if data['unit_price'] < 0:
+            raise serializers.ValidationError({'unit_price': 'Price cannot be negative'})
         return data

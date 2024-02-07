f47f04d52d59fbeea35e905436f27e9c3d9d8854
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-07 23:11:22 +0000

    settings gor little lemon project

--- little_lemon_api_project/LittleLemon/settings.py
+++ little_lemon_api_project/LittleLemon/settings.py
@@ -135,7 +135,17 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 SIMPLE_JWT = {

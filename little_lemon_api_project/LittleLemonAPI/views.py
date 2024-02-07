0679ec07953fd23b90b575d40f0a101e4f1e2700
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-07 23:13:06 +0000

    views for endpoints

--- little_lemon_api_project/LittleLemonAPI/views.py
+++ little_lemon_api_project/LittleLemonAPI/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import (
     api_view,
-    permission_classes,
-    throttle_classes
+    throttle_classes,
 )
 from rest_framework import status
 from django.contrib.auth.models import User
@@ -11,14 +10,28 @@ from .models import (
     MenuItem,
 )
 from django.core.exceptions import ObjectDoesNotExist
-from .serializers import MenuItemSerializer, CartSerializer
+from .serializers import MenuItemSerializer, CartSerializer, OrderSerializer
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.throttling import UserRateThrottle
 
 
 @api_view(['GET', 'POST'])
 def menu_item(request):
     if request.method == 'GET':
         menuItems = MenuItem.objects.all()
-        return Response(menuItems, status=status.HTTP_200_OK)
+        
+        # Filtering
+        category = request.query_params.get('category', None)
+        if category is not None:
+            menuItems = menuItems.filter(category=category)
+        many = menuItems.count() > 1
+        
+        # Pagination
+        paginator = PageNumberPagination()
+        paginated_menuItems = paginator.paginate_queryset(menuItems, request)
+        
+        serializer = MenuItemSerializer(paginated_menuItems, many=many)
+        return paginator.get_paginated_response(serializer.data)
     
     if request.method == 'POST':
         if request.user.groups.filter(name='Manager').exists():
@@ -138,3 +151,83 @@ def cart_menu_item(request):
             return Response(status=status.HTTP_204_NO_CONTENT)
         except ObjectDoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
+        
+
+@api_view(['GET', 'POST'])
+@throttle_classes([UserRateThrottle])
+def order(request):
+    if request.method == 'GET':
+        try:
+            orders = Order.objects.filter(user=request.user)
+            many = orders.count() > 1
+            serializer = OrderSerializer(orders, many=many)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'POST':
+        serializer = OrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            
+            # Get cart items for the current user
+            cart_items = Cart.objects.filter(user=request.user)
+            
+            # Add cart items to the order items table
+            for cart_item in cart_items:
+                order_item = OrderItem.objects.create(
+                    order=serializer.instance,
+                    menu_item=cart_item.menu_item,
+                    quantity=cart_item.quantity
+                )
+                order_item.save()
+            
+            # Delete all items from the cart for this user
+            cart_items.delete()
+            
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    
+@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+def order_detail(request, orderId):
+    try:
+        order = Order.objects.get(pk=orderId)
+    except Order.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serializer = OrderSerializer(order)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    if request.method == 'DELETE':
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    if request.method == 'PUT' and request.user.groups.filter(name='Manager').exists():
+        delivery_crew_id = request.data.get('deliveryCrewId')
+        status = request.data.get('status')
+        
+        if delivery_crew_id is not None:
+            try:
+                delivery_crew = User.objects.get(pk=delivery_crew_id)
+                order.delivery_crew = delivery_crew
+            except User.DoesNotExist:
+                return Response({'error': 'Invalid delivery crew ID'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if status is not None:
+            if status == 0 or status == 1:
+                order.status = status
+            else:
+                return Response({'error': 'Invalid status value'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        order.save()
+        return Response({'message': 'Order updated successfully'}, status=status.HTTP_200_OK)
+    
+    if request.method in ['PUT', 'PATCH']:
+        serializer = OrderSerializer(order, data=request.data, partial=request.method == 'PATCH')
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    

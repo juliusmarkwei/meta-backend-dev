8e15e74e36a00a46c832cc4d0ec1c88316b583c3
Julius Markwei <juliusmarkwei2000@gmail.com>
2024-02-07 23:12:22 +0000

    serializers

--- little_lemon_api_project/LittleLemonAPI/serializers.py
+++ little_lemon_api_project/LittleLemonAPI/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import MenuItem, Cart
+from .models import MenuItem, Cart, Order
 
 class MenuItemSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,7 +19,7 @@ class MenuItemSerializer(serializers.ModelSerializer):
         return data
     
 
-class CartSerializer(serializers.ModelsSerializer):
+class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = ['id', 'user', 'menuitem', 'quantity', 'unit_price', 'price']
@@ -36,4 +36,22 @@ class CartSerializer(serializers.ModelsSerializer):
             raise serializers.ValidationError({'price': 'Price cannot be negative'})
         if data['unit_price'] < 0:
             raise serializers.ValidationError({'unit_price': 'Price cannot be negative'})
+        return data
+    
+    
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = ['id', 'user', 'delivery_crew', 'status', 'total', 'date']
+        read_only_fields = ['id']
+    
+    def validate(self, data):
+        required_fields = ['user', 'status', 'total', 'date']
+        
+        for field in required_fields:
+            if field not in data:
+                raise serializers.ValidationError({field: 'This field is required'})
+        
+        if data['total'] < 0:
+            raise serializers.ValidationError({'total': 'Total cannot be negative'})
         return data
